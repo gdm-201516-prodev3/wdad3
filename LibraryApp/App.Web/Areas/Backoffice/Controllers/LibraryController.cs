b27b6d2c0a2fe1235453eb8c68eaeadf94c996b9
drdynscript <drdynscript@gmail.com>
2015-10-19 10:17:59 +0200

    Update App

--- LibraryApp/App.Web/Areas/Backoffice/Controllers/LibraryController.cs
+++ LibraryApp/App.Web/Areas/Backoffice/Controllers/LibraryController.cs
@@ -9,9 +9,28 @@ namespace App.Web.Areas.Backoffice.Controllers
     [Area("Backoffice")]
     public class LibraryController : CommonController
     {
+        [HttpGet]
         public IActionResult Index()
         {
             var model = _libraryContext.Libraries.AsEnumerable().OrderBy(m => m.Name);
+            
+            return View(model);
+        }
+        
+        [HttpGet]
+        public IActionResult Edit(Int16? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(400);
+            }
+            
+            var model = _libraryContext.Libraries.FirstOrDefault(m => m.Id == id);
+            
+            if(model == null)
+            {
+                return RedirectToAction("Index");
+            }
             
             return View(model);
         }
